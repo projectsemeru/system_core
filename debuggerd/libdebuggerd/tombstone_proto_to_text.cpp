1cc67e2048facdb6af0d8052d94db44cf5ee86c2
Scott Mertz <scott@cyngn.com>
2016-05-25 15:47:42 -0700

    debuggerd: add Edith version to tombstones
    Change-Id: I7822a2e133a2f326ad2d8da8f79b0064344d59bf
    
    debuggerd: Rebrand tombstone header to Edith
    
    [harryyoud]: Use new props for 15.1
    [rashed]: Bah... use GetProperty
    [mkbestas]: Rewrite for T
    
    Change-Id: Idd08c2eb7e395b464b1510742bf52833f465db08

--- debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
+++ debuggerd/libdebuggerd/tombstone_proto_to_text.cpp
@@ -700,6 +700,7 @@ static void print_guest_thread(CallbackType callback, SymbolizeCallbackType symb
 bool tombstone_proto_to_text(const Tombstone& tombstone, CallbackType callback,
                              SymbolizeCallbackType symbolize, bool display_sp) {
   CBL("*** *** *** *** *** *** *** *** *** *** *** *** *** *** *** ***");
+  CBL("Edith Version: '%s'", tombstone.edith_version().c_str());
   CBL("Build fingerprint: '%s'", tombstone.build_fingerprint().c_str());
   if (!tombstone.kernel_release().empty()) {
     CBL("Kernel Release: '%s'", tombstone.kernel_release().c_str());
