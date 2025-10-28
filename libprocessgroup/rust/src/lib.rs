10de2c7e408b35b2c74024a02316de8cbdc76684
Chris Wailes <chriswailes@google.com>
2025-10-27 17:42:54 -0700

    Use cxx crate to bind additional functions.
    This CL adds bindings for the following functions:
    * CgroupGetAttributePathForProcess
    * killProcessGroup
    * SetTaskProfiles
    
    Test: m libprocessgroup_rust
    Test: m zygote_all
    Bug: 455587171
    Change-Id: Iec12a8a51bfaf444f03727dc0683a868f131b6df

--- libprocessgroup/rust/src/lib.rs
+++ libprocessgroup/rust/src/lib.rs
@@ -14,68 +14,8 @@
 // limitations under the License.
 
 //! Bindings for Android's libprocessgroup
-//!
-//! Mangled C++ names provided to `link_name` attributes were suggested by the
-//! linker when no name was provided.  The format for functions that take no
-//! parameters and return no values is:
-//! `'_Z' + Min API Level + Function Name + 'v'`.
 
 #![cfg(target_os = "android")]
 
-use core::ffi::c_int;
-
-/// Android scheduling policy constants
-///
-/// See: system/core/libprocessgroup/include/processgroup/sched_policy.h
-#[repr(C)]
-pub enum SchedPolicy {
-    /// Default scheduling policy for non-system processes
-    Default = -1,
-    /// Scheduling policy for applications in the background
-    Background = 0,
-    /// Scheduling policy for applications in the foreground
-    Foreground = 1,
-    /// Scheduling policy for system services
-    System = 2,
-    /// Scheduling policy for audio threads belonging to applications
-    AudioApp = 3,
-    /// Scheduling policy for audio threads belonging to system services
-    AudioSys = 4,
-    /// Scheduling policy for "Top Apps"
-    TopApp = 5,
-    /// Scheduling policy for real-time applications
-    RTApp = 6,
-    /// Scheduling policy for restricted applications
-    Restricted = 7,
-    /// Scheduling policy for foregrounded application windows
-    ForegroundWindow = 8,
-}
-
-impl SchedPolicy {
-    /// Default scheduling policy for system processes
-    #[allow(non_upper_case_globals)]
-    pub const SystemDefault: Self = Self::Foreground;
-}
-
-// Temporarily allow dead code as the rest of the module is defined in
-// following commits.
-unsafe extern "C" {
-    /// Check to see if cpusets have been enabled on the system
-    pub safe fn cpusets_enabled() -> bool;
-
-    /// Drop the FD cache for the cgroup path.
-    #[link_name = "_Z31DropTaskProfilesResourceCachingv"]
-    pub safe fn drop_task_profiles_resource_caching();
-
-    /// Set the cpuset policy for the specified process.  A TID of 0 means
-    /// that the policy will be applied to the calling thread.
-    ///
-    /// This function takes no pointer arguments and is thread-safe.
-    pub safe fn set_cpuset_policy(tid: libc::pid_t, policy: SchedPolicy) -> c_int;
-
-    /// Set the scheduling policy for the specified process.  A TID of 0
-    /// means that the policy will be applied to the calling thread.
-    ///
-    /// This function takes no pointer arguments and is thread-safe.
-    pub safe fn set_sched_policy(tid: libc::pid_t, policy: SchedPolicy) -> c_int;
-}
+pub mod processgroup;
+pub mod sched;
