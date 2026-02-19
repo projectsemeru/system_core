ac641d4505375524e51803c0cce6d74bd98ec38b
Akshay Subramanian <akshaysu@google.com>
2026-02-19 12:47:40 +0000

    provisioning_tool(attestation): Add algorithm parsing logic
    This change adds logic to parse algorithm from attestation
    xml file
    
    Bug: 478175656
    Test: atest keymint_provisioning_tool_tests
    Change-Id: Ib3a30e0dc89e3353fd38f1e7146a0f6873c9861d

--- trusty/keymint/provisioning/src/attestation.rs
+++ trusty/keymint/provisioning/src/attestation.rs
@@ -14,16 +14,61 @@
 
 //! Parse and Fetch attestation keys data
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use std::fs;
+use xml::{
+    attribute::OwnedAttribute,
+    reader::{EventReader, XmlEvent},
+};
 
+#[derive(Debug, PartialEq, Eq)]
 pub(crate) struct AttestationKey {
     pub(crate) algorithm: String,
     pub(crate) key: Vec<u8>,
     pub(crate) certs: Vec<Vec<u8>>,
 }
 
-// TODO(b/478175656): Add logic to parse attestation keys xml file
-pub(crate) fn get_attestation_keys(_file: fs::File) -> Result<Vec<AttestationKey>> {
-    Ok(vec![AttestationKey { algorithm: "rsa".to_string(), key: vec![], certs: vec![] }])
+pub(crate) fn get_attestation_keys(file: fs::File) -> Result<Vec<AttestationKey>> {
+    let mut xml_events_reader = EventReader::new(file);
+    let mut attestation_keys = vec![];
+
+    loop {
+        match xml_events_reader.next()? {
+            XmlEvent::StartElement { name, attributes, .. } => {
+                if name.local_name.as_str() == "Key" {
+                    let algorithm = get_key_algorithm(attributes)?;
+                    let attestation_key = AttestationKey { algorithm, key: vec![], certs: vec![] };
+                    attestation_keys.push(attestation_key);
+                }
+            }
+            XmlEvent::EndDocument => break,
+            _ => continue,
+        }
+    }
+
+    Ok(attestation_keys)
+}
+
+fn get_key_algorithm(attributes: Vec<OwnedAttribute>) -> Result<String> {
+    attributes
+        .into_iter()
+        .find(|attribute| attribute.name.borrow().local_name == "algorithm")
+        .map(|attribute| attribute.value)
+        .ok_or(anyhow!("Missing algorithm attribute"))
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn validate_get_attestation_keys() -> Result<()> {
+        let file = fs::File::open("set_attestation_key/keymaster_soft_attestation_keys.xml")?;
+        let attestation_keys = get_attestation_keys(file)?;
+        assert_eq!(attestation_keys.len(), 2);
+        let expected_attestation_key_0 =
+            AttestationKey { algorithm: "rsa".to_string(), key: vec![], certs: vec![] };
+        assert_eq!(attestation_keys[0], expected_attestation_key_0);
+        Ok(())
+    }
 }
