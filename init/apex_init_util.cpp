c1d2414000633ac73500947dac100f7486b866ec
Jooyung Han <jooyung@google.com>
2026-03-10 10:30:19 +0900

    init: Disable mount-before-data on DSU/GSI
    When booting into DSU, the /metadata partition is the same as the
    original Android. For example, apexd may try mount APEXes early before
    /data based on the existence of /metadata/apex/config/mount_before_data
    even though GSI has Compressed APEXex which require /data for mounting.
    
    Until we have a good resolution to isolate /metadata/apex for DSU/GSI,
    disabling the feature for now.
    
    Note: this is a modified version of ag/38589237. SetProperty should be
    called after the property service is started.
    
    Bug: 483904646
    Flag: EXEMPT BUGFIX
    Test: $ atest CtsDeviceConfigTestCases -- --template:map preparers=template/preparers/dsu-preparer --extra-file system-img.zip=${ANDROID_PRODUCT_OUT}
    Change-Id: Ief609822943d0ce890974e7dfccc424a032a2dfc

--- init/apex_init_util.cpp
+++ init/apex_init_util.cpp
@@ -26,6 +26,7 @@
 #include <android-base/properties.h>
 #include <android-base/result.h>
 #include <android-base/strings.h>
+#include <libgsi/libgsi.h>
 
 #include "action_manager.h"
 #include "init.h"
@@ -161,7 +162,14 @@ Result<void> ParseRcScriptsFromAllApexes(bool is_default_mnt_ns) {
 
 bool CanMountApexBeforeData() {
     // Can't mount APEXes before /data without FIEMAP support
-    if (!base::GetBoolProperty("apexd.config.use_fiemap", true)) {
+    if (!base::GetBoolProperty(kApexdUseFiemapProp, true)) {
+        return false;
+    }
+
+    // Disable the feature for DSU/GSI device to avoid using /metadata/apex which is for
+    // the original Android.
+    // TODO(b/487508309)
+    if (gsi::IsGsiRunning()) {
         return false;
     }
 
